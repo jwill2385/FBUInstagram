30ae69cbc4165c2d8da0daa56690676fa840bae4
Jadal Williams <jwill21@fb.com>
2018-07-10 16:00:15 -0700

    added a sign up feature

--- app/src/main/java/me/jwill2385/instagram/MainActivity.java
+++ app/src/main/java/me/jwill2385/instagram/MainActivity.java
@@ -7,16 +7,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private  Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         etUsername = (EditText) findViewById(R.id.etUsername);
         etPassword = (EditText) findViewById(R.id.etPassword);
         btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp) ;
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +38,18 @@ public class MainActivity extends AppCompatActivity {
             final String password = etPassword.getText().toString();
 
             login(username, password);
+            }
+
+
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                signUp(username, password);
             }
 
 
@@ -57,6 +73,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void signUp(String username, String password){
+
+        // Create the ParseUser
+        ParseUser user = new ParseUser();
+// Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+        
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                    Toast.makeText(MainActivity.this, "New Account Created", Toast.LENGTH_SHORT).show();
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 }
